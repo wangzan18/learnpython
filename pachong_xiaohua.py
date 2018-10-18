a98736a305af94d2de6cf9adc4a7862dbec7013b
王赞 <zan.wang@comratings.com>
2018-10-18 11:51:28 +0800

    python爬取校花网

--- pachong_xiaohua.py
+++ pachong_xiaohua.py
@@ -6,7 +6,7 @@ import requests
 respon = requests.get('http://www.xiaohuar.com/v/')
 
 # 在响应的内容中，通过正则表达式匹配出url
-urls = re.findall(r'class="items".*?href="(.*?)"',respon.text,re.S)
+urls = re.findall(r'class="items".*?href="(.*?)"', respon.text, re.S)
 url = urls[5]
 result = requests.get(url)
 mp4_url = re.findall(r'id="media"')
