d0d1d1fc8e0d540df08aee3065bbd35933928ace
王赞 <zan.wang@comratings.com>
2018-10-18 11:49:28 +0800

    统计每个目录图片视频数量

--- count_pic_daily.py
+++ count_pic_daily.py
@@ -1,28 +1,41 @@
+#!/usr/bin/python3.6
+# -*- coding: UTF-8 -*-
+
+# wangzan18@126.com
+# 2018-10-16
+
 import os
 
 # 图片存储目录
-path='C:\\Users\\Water\\Downloads\\vivo\\'
+path = 'C:\\Users\\Water\\Downloads\\vivo\\'
 
 # 初始化变量
 file = []
 video = 0
 jpg = 0
 
-# 对文件夹循环
-for imei in os.listdir(path):
-    # 对文件夹内部日期循环
-    for picdate in os.listdir(path+imei):
-        # 统计某日期下面所有图片总量
-        for root,dirs,files in os.walk(path+imei+"\\"+picdate):
-            file += files
-        # 对某个日期下面的图片和视频进行帅选，并统计数量
-        for a in file:
-            if os.path.splitext(a)[1] == '.mp4':
-                video += 1
-            else:
-                jpg += 1
-        # 打印文件夹名，日期名，图片数量，视频数量
-        print(imei,picdate,jpg,video)
-        video = 0
-        jpg = 0
-        file = []
+
+def pic_count():
+    """
+    统计目录下面的图片数量，及其子目录下面的图片数量，视频数量。
+    """
+    for imei in os.listdir(path):
+
+        for picdate in os.listdir(path+imei):
+
+            for root, dirs, files in os.walk(path+imei+"\\"+picdate):
+                file += files
+
+            for a in file:
+                if os.path.splitext(a)[1] == '.mp4':
+                    video += 1
+                else:
+                    jpg += 1
+
+            print(imei, picdate, jpg, video)
+            video = 0
+            jpg = 0
+            file = []
+
+
+pic_count()
