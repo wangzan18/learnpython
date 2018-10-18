ac6897d105f986690b7e8c2c9057ad9a3768c597
王赞 <zan.wang@comratings.com>
2018-10-18 11:49:48 +0800

    向钉钉发送卡片消息

--- dingding_text.py
+++ dingding_text.py
@@ -11,29 +11,35 @@ import sys
 headers = {'Content-Type': 'application/json;charset=utf-8'}
 
 # 获取群机器人接口
-api_url = "https://oapi.dingtalk.com/robot/send?access_token=787ff7bd0e71074e297bd7853fad956a6dc3ef5a82150548acd1c0b9de13fb26"
+api_url = "https://oapi.dingtalk.com/robot/send?access_token=" \
+          "787ff7bd0e71074e297bd7853fad956a6dc3ef5a82150548acd1c0b9de13fb26"
 
 
-def msg(title,text):
-    # 接口消息类型，具体可以查看官方接口文档
+def msg(title, text):
+    """
+    向钉钉接口发送消息
+    :param title: 消息的名称
+    :param text:  消息的内容
+    """
     json_text= {
      "msgtype": "link",
-        "link": {
-            "title" : title,
+     "link": {
+            "title": title,
             "text": text,
-            "messageUrl" : "http://www.huijifen.cn"
+            "messageUrl": "http://www.wzlinux.cn"
         },
-        "at": {
+     "at": {
             "atMobiles": [
-                "18817511223"
+                "18817511111"
             ],
             "isAtAll": False
         }
     }
-    # 向接口发送数据，并返回处理结果
-    print(requests.post(api_url,json.dumps(json_text),headers=headers).content)
+
+    print(requests.post(api_url, json.dumps(json_text), headers=headers).content)
+
 
 if __name__ == '__main__':
-    title = sys.argv[1]   # 获取传递的第一个参数
-    text = sys.argv[2]
-    msg(title,text)
+    a = sys.argv[1]
+    b = sys.argv[2]
+    msg(a, b)
