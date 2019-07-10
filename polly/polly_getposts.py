88afb76a64cf5ff19e199156227030b4effeec20
王赞 <zan.wang@transsion.com>
2019-07-10 17:44:15 +0800

    强化代码

---
+++ polly/polly_getposts.py
@@ -0,0 +1,20 @@
+import boto3
+import os
+from boto3.dynamodb.conditions import Key
+
+
+def lambda_handler(event, context):
+
+    postId = event["postId"]
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['DB_TABLE_NAME'])
+
+    if postId == "*":
+        items = table.scan()
+    else:
+        items = table.query(
+            KeyConditionExpression=Key('id').eq(postId)
+        )
+
+    return items["Items"]
