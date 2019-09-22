5672f42848f2512ce3fb85daf7b5ab460f83f513
王赞 <zan.wang@transsion.com>
2019-09-22 11:41:08 +0800

    优化

--- application/google_translate.py
+++ application/google_translate.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from googletrans import Translator
-translator = Translator(service_urls=['translate.google.com'])
-f1 = open("C:\\Users\\Administrator\\Downloads\\word1.csv", 'a')
-with open("C:\\Users\\Administrator\\Downloads\\word.csv", 'r') as file:
-    data_lines = file.readlines()
-for line in data_lines:
-    line = line.strip('\n')
-    text = translator.translate(line, src='en', dest='zh-cn').text
-    f1.write(line + "," + text + '\n')
+# Imports the Google Cloud client library
+from google.cloud import translate
 
+# Instantiates a client
+translate_client = translate.Client.from_service_account_json('C:\\Users\\zan.wang\\'
+                                                              'Downloads\\transsion-tecno-ef0340407413.json')
+
+# The target language
+target = 'zh-cn'
+
+f1 = open("C:\\Users\\zan.wang\\Downloads\\word1.txt", 'a')
+with open("C:\\Users\\zan.wang\\Downloads\\word.txt", 'r') as file:
+    data_lines = file.readlines()
+
+for text in data_lines:
+    text = text.strip('\n')
+    translation = translate_client.translate(text, target_language=target)
+    print(text + "," + translation['translatedText'] + '\n')
+    f1.write(text + "," + translation['translatedText'] + '\n')
 
 file.close()
 f1.close()
+
 
